b1189534e45e783b96734e808756532489124f3f
Talman Aarne J <aarne.talman@helsinki.fi>
2018-08-27 15:55:07 +0300

    cleaned up training scripts

--- train_infersent.sh
+++ train_infersent.sh
@@ -1,19 +1,6 @@
 #!/bin/bash -l
-#SBATCH -J SNLI_InferSent_4096D
-#SBATCH -o out_InferSent_4096D_%J.txt
-#SBATCH -e err_InferSent_4096D_%J.txt
-#SBATCH -p gpu
-#SBATCH -N 1
-#SBATCH -n 1
-#SBATCH -t 08:00:00
-#SBATCH --mem-per-cpu=16000
-#SBATCH --gres=gpu:p100:1
-# run command
-module purge
-module load gcc cuda python-env/3.5.3-ml
-module list
 
-srun python3 train.py \
+python3 train.py \
   --epochs 20 \
   --batch_size 64 \
   --corpus snli \
@@ -32,9 +19,3 @@ srun python3 train.py \
   --lr_reduction_factor 0.2 \
   --save_path results \
   --seed 1234
-
-# This script will print some usage statistics to the
-# end of file: output.txt
-# Use that to improve your resource request estimate
-# on later jobs.
-used_slurm_resources.bash
